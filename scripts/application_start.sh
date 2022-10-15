ad2e85a8c6f4f4fd765f0b6da5d45d568b8a499f
ud17 <upandya1297@gmail.com>
2022-10-14 23:29:55 -0400

    node version fix

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,7 +12,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-npm install
+npm install 16.17.1
 
 #start our node app in the background
 node app.js > app.out.log 2> app.err.log < /dev/null & 
